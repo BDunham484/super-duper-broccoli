80e382421dea6fdfd5322574ce4444bc4ab5590f
ThereWolf <bdunham484@gmail.com>
2023-12-21 21:18:35 -0600

    classwork

--- 01-Class-Content/09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,11 +1,14 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
- 
+  title: 'Equimanthorn',
+  artist: 'Bathory',
+  album: '"Under the Sign of the Black Mark"',
+  music: 'Black Metal',
 };
 
 // Write code between the backticks tags to output the data from the music object above.
-const songSnippet = ``;
+const songSnippet = `My favorite song by ${music.artist} is ${music.title} off of ${music.album}.`;
 
 console.log(songSnippet)
 
--- 01-Class-Content/09-NodeJS/01-Activities/16-Stu_maths/Unsolved/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/16-Stu_maths/Unsolved/index.js
@@ -1,7 +1,22 @@
 // TODO: Import `maths.js`
+const maths = require('./maths.js');
 
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
+// const operation = process.argv[2];
+// const numOne = process.argv[3];
+// const numTwo = process.argv[4]
+
+const [operation, numOne, numTwo] = process.argv.slice(2);
+
+// console.log(operation)
+// console.log(numOne)
+// console.log(numTwo)
 
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 // and each `case` uses the corresponding `maths` method
 // to perform each math operation on the two numbers, `numOne` and `numTwo`
+// console.log(maths[operation](+numOne, +numTwo))
+
+const result = maths?.[operation]?.(+numOne, +numTwo)
+
+console.log(result)
