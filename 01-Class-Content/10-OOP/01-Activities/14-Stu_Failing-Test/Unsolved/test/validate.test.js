f04c267286f631872f49df1cb41c6a3c6b29d6fd
ThereWolf <bdunham484@gmail.com>
2024-01-08 19:33:32 -0600

    classwork

--- 01-Class-Content/10-OOP/01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
+++ 01-Class-Content/10-OOP/01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
@@ -1,5 +1,16 @@
 // TODO: Import the Validate class.
+import Validate from "../validate";
 
 // TODO: Describe a testing suite for checking the functionality of the Validate class.
+describe('Validate', () => {
 
 // TODO: Write a test that asserts that isPassword() returns false when passed an empty string.
+it ('should return false when passed an empty string', () => {
+    const str = '';
+    const result = new Validate().isPassword(str);
+
+    expect(result).toEqual(false);
+})
+})
+
+
