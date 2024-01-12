a9616956880ae7b52b541fdbed11e130d6c1bbc9
ThereWolf <bdunham484@gmail.com>
2024-01-11 21:27:51 -0600

    classs

--- 01-Class-Content/11-Express/01-Activities/02-Stu_Setup/Unsolved/server.js
+++ 01-Class-Content/11-Express/01-Activities/02-Stu_Setup/Unsolved/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+// import builit-in Node.js package path to resolve path of files that are located on the server
 const path = require('path');
 
 const app = express();
 const PORT = 3001;
 
 // TODO: Invoke app.use() and serve static files from the '/public' folder
+// static middleware pointing to the public folder
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
--- 01-Class-Content/11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ 01-Class-Content/11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,11 +1,18 @@
 // TODO: Import express
-
+const express = require('express');
+const path = require('path');
 // TODO: Import 'terms.json' file
+const terms = require('./terms.json');
+
+const app = express();
 const PORT = 3001;
 
 // TODO: Initialize app variable
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
+app.get('/api', (req, res) => {
+  res.json(terms);
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
