04b8bd6ba6c159e571a7abb8e048cd6231650920
ThereWolf <bdunham484@gmail.com>
2024-01-17 19:25:06 -0600

    class

--- 01-Class-Content/11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
+++ 01-Class-Content/11-Express/01-Activities/12-Stu_Insomnia/Unsolved/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 // TODO: Require the json file located in `/db`
+const repos = require('./db/repos.json');
 
+const app = express();
+const PORT = 3001;
 // TODO: Create an `app` variable set to the value of `express()`
 
 app.get('/', (req, res) => {
@@ -10,5 +13,9 @@ app.get('/', (req, res) => {
 });
 
 // TODO: Create a GET route for `/api` that will return the content of our json file
+app.get('/api', (req, res) => res.json(repos));
 
 // TODO: Have the app listen on port 3001
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
