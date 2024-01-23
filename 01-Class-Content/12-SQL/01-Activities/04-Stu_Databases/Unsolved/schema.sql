a73b622f6780e43f6f635b1372bc2e283dde57be
ThereWolf <bdunham484@gmail.com>
2024-01-22 19:34:17 -0600

    classwork

--- 01-Class-Content/12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
+++ 01-Class-Content/12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,6 @@
 -- Write code here or enter directly in MySQL shell --
+-- Drops the sample_db if it exists currently --
+DROP DATABASE IF EXISTS sample_db;
+DROP DATABASE IF EXISTS books_db;
+-- Creates the sample_db database --
+CREATE DATABASE books_db;
