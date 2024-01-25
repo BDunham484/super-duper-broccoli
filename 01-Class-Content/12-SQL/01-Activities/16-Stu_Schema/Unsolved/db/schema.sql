5c3643292549b2b2a57502786c9d524ee76601d2
ThereWolf <bdunham484@gmail.com>
2024-01-24 21:01:25 -0600

    classwork

--- 01-Class-Content/12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 01-Class-Content/12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,17 @@
--- Write your schema here --
+drop database if exists class_db;
+create database class_db;
+
+use class_db;
+
+create table products(
+    id int not null,
+    product_name varchar(30) not null,
+    category_name varchar(100) not null,
+    price int not null,
+    in_stock boolean
+);
+
+create table categories(
+    id int not null,
+    category_name varchar(30) not null
+);
--- 01-Class-Content/12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ 01-Class-Content/12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,21 @@
 -- Write your Schema Here -- 
+if exists drop database work_db;
+create database work_db; 
+
+use work_db;
+
+create table customers (
+    id int not null primary key,
+    first_name varchar(30),
+    last_name varchar(30),
+
+);
+
+create table customer_order (
+    id int not null,
+    customer_id int not null,
+    order_details text not null
+    foreign key (customer_id)
+    REFERENCES customers(id)
+    on delete set null
+)
