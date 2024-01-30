f416ec688a50a377984f866bf61734a4869621f0
ThereWolf <bdunham484@gmail.com>
2024-01-29 18:22:02 -0600

    classwork

--- 01-Class-Content/12-SQL/01-Activities/26-Stu_JOIN/Unsolved/db/query.sql
+++ 01-Class-Content/12-SQL/01-Activities/26-Stu_JOIN/Unsolved/db/query.sql
@@ -1,2 +1,2 @@
 -- Add your code below and execute file in MySQL Shell --
-select * from favorite_books fb inner join book_prices bp on fb.book_price = bp.id order by fb.id;
+select fb.book_name as Title, bp.price as Price from favorite_books fb join book_prices bp on fb.book_price = bp.id order by bp.price;
