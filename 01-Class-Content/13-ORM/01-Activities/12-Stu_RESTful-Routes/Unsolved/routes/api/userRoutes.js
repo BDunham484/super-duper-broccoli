5962a41e3124949fc5f0e6ad12374d1c3f61a7a9
ThereWolf <bdunham484@gmail.com>
2024-01-31 19:19:50 -0600

    classwork

--- 01-Class-Content/13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
+++ 01-Class-Content/13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
@@ -1,16 +1,40 @@
 const router = require('express').Router();
 const User = require('../../models/User');
 
+// changelog-start
+router.get('/', (req, res) => {
+  // Get all books from the book table
+  User.findAll().then((userData) => {
+    res.json(userData);
+  });
+});
+// changelog-end
+
 // TODO: Use try/catch to catch errors
 // TODO: Return the appropriate HTTP status codes
+try {
+  router.get('/:id', async (req, res) => {
+    const userData = await User.findByPk(req.params.id)
+
+    if (!userData) {
+      res.status(404).json(userData);
+      return
+    }
+
+    res.status(200).json(userData);
+  })
+} catch (err) {
+  console.error(err);
+  res.status(500).json(err);
+}
 
 // GET a user
-router.get('/:id', async (req, res) => {
-  const userData = await User.findByPk(req.params.id).catch((err) =>
-    res.json(err)
-  );
-  res.json(userData);
-});
+// router.get('/:id', async (req, res) => {
+//   const userData = await User.findByPk(req.params.id).catch((err) =>
+//     res.json(err)
+//   );
+//   res.json(userData);
+// });
 
 // UPDATE a user
 router.put('/:id', async (req, res) => {
